2a7f37bb183b6d5921184107360a789ed68a856a
muzaffarmhm <muzaffarmhm@gmail.com>
2021-11-10 11:56:35 +0530

    Adding ejs to the project

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
 
 app.listen(3000,()=>{
-    res.send("App Listening at 3000")
+    console.log("App Listening at 3000...")
 })
 
 app.get('/',(req,res)=>{
-    res.send('Inshivir Home Page')
+    res.render('home')
 })
